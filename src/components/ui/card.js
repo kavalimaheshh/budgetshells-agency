e226808895643b2eaf7e38f32815d4877ab3e11b
kavalimaheshh <maheshkavali369@gmail.com>
2025-07-03 23:36:41 +0530

    Delete src directory

--- src/components/ui/card.js
+++
@@ -1,1 +0,0 @@
-export function Card({ children }) { return <div className='bg-white rounded-lg shadow'>{children}</div>; } export function CardContent({ children }) { return <div>{children}</div>; }
